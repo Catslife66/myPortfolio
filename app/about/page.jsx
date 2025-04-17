fe25ef8eeca83dd744ec8d307a3f599d3bf4aee6
Catslife66 <josie58@live.cn>
2025-04-17 14:10:50 +0100

    upload static content

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,5 @@
 "use client";
 
-import gsap from "gsap";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-
-import { experienceContent, introToExpContent } from "../data/textContent";
-import { useGSAP } from "@gsap/react";
-import { Observer } from "gsap/Observer";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
 export default function page() {
   return <></>;
 }
---
+++ app/data/textContent.js
@@ -0,0 +1,145 @@
+export const heroContent = {
+  paragraph:
+    "A self-taught Programmer with a passion for Web development and a strong background in Marketing.",
+  h4: "aka Josie",
+  btn: "scroll to explore more",
+};
+
+export const experienceIntro = {
+  content:
+    "I’m a self-taught developer with a background in branding, storytelling, and digital strategy. Before I started writing code, I was crafting brand experiences-thinking deeply about how things look, feel, and function. That creative mindset still drives my work today. My path into programming grew from a curiosity to build, to understand how things work behind the screen. Since then, I’ve become fluent in tools like React, Django, and Docker, and I’m continually exploring the intersection of thoughtful design and scalable architecture. That blend of creativity and systems thinking began in my previous career and that’s where the story starts.",
+  highlightWords: [
+    "self-taught",
+    "experiences-thinking",
+    "digital",
+    "mindset",
+    "programming",
+    "creativity",
+  ],
+};
+
+export const introToExpContent = {
+  h2: "I bring over 5 years of experience in marketing and communications within the hospitality industry, where I led digital initiatives, developed brand systems, and executed cross-functional campaigns for international hotel groups.",
+  highlightWords: ["marketing", "hospitality", "brand"],
+  class: "introToExpContent",
+};
+
+export const experienceContent = [
+  {
+    id: 1,
+    time: "May 2014 to Jul 2015",
+    workplace: "Howard Johnson Hotel Haikou",
+    position: "Marketing & Communication",
+    image: {
+      src: "assets/images/bird.jpg",
+      alt: "placeholder",
+    },
+  },
+  {
+    id: 2,
+    time: "Aug 2015 to Oct 2016",
+    workplace: "HUALUXE Seaview Haikou",
+    position: "Marketing & Communication",
+    image: { src: "assets/images/ph_ver1.jpg", alt: "sample" },
+  },
+  {
+    id: 3,
+    time: "Mar 2017 to Jun 2017",
+    workplace: "LN Hotel Boutique Foshan",
+    position: "Marketing & Communication",
+    image: { src: "assets/images/ph_ver2.jpg", alt: "text" },
+  },
+  {
+    id: 4,
+    time: "Aug 2017 to Dec 2020",
+    workplace: "China Southern Airlines Hotels",
+    position: "Branding & Positioning",
+    image: { src: "assets/images/ph_ver3.jpg", alt: "see" },
+  },
+];
+
+export const themeColours = {
+  main: "#645394",
+  shade: "#2B233F",
+  tint: "#DDD9E9",
+};
+
+export const expToSkillContent = {
+  h2: "I like building things that are useful, usable, and occasionally just for fun. These projects highlight how I think and solve problems—from intuitive interfaces and smooth interactions to solid architecture and real-world deployment.",
+  class: "expToSkillContent",
+};
+
+export const aboutImgGroup = [
+  { type: "image", src: "assets/images/pic_1.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_2.jpg", alt: "text" },
+  { type: "text", text: "about" },
+  { type: "image", src: "assets/images/pic_3.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_4.jpg", alt: "text" },
+];
+
+export const meImgGroup = [
+  { type: "image", src: "assets/images/pic_5.jpg", alt: "text" },
+  { type: "text", text: "me" },
+  { type: "image", src: "assets/images/pic_6.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_7.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_8.jpg", alt: "text" },
+];
+
+export const programmingImgGroup = [
+  { type: "image", src: "assets/images/pic_1.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_2.jpg", alt: "text" },
+  { type: "text", text: "programming" },
+  { type: "image", src: "assets/images/pic_3.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_4.jpg", alt: "text" },
+];
+
+export const skillsetImgGroup = [
+  { type: "image", src: "assets/images/pic_5.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_6.jpg", alt: "text" },
+  { type: "image", src: "assets/images/pic_7.jpg", alt: "text" },
+  { type: "text", text: "skillset" },
+  { type: "image", src: "assets/images/pic_8.jpg", alt: "text" },
+];
+
+export const skillsetContent = [
+  {
+    title: "Full-Stack Development",
+    description:
+      "I specialise in building scalable web applications using React, Redux, and JavaScript for dynamic front-end experiences, while leveraging Django and Node.js for robust back-end solutions. With experience in PostgreSQL and MongoDB, I design efficient database structures to support data-driven applications.",
+    skills: [
+      "React, Redux, JavaScript, Node.js",
+      "Django, Django Rest Framework",
+      "PostgreSQL, MongoDB",
+    ],
+    mediaSrc: "assets/videos/video1.mov",
+  },
+  {
+    title: "UI/UX & 3D Web Development",
+    description:
+      "Combining my strong design intuition from a marketing background with technical expertise, I create visually engaging interfaces using Tailwind CSS and Bootstrap 5. I also have foundational experience in Three.js (R3F, Drei) and Spline, allowing me to integrate 3D elements and interactive experiences into web applications. specialize in building scalable web applications using React, Redux, and JavaScript for dynamic front-end experiences, while leveraging Django and Node.js for robust back-end solutions. With experience in PostgreSQL and MongoDB, I design efficient database structures to support data-driven applications.",
+    skills: [
+      "Three.js (R3F, Drei), Spline",
+      "Tailwind CSS, Bootstrap 5",
+      "Strong design intuition from marketing experience<",
+    ],
+    mediaSrc: "assets/videos/video1.mov",
+  },
+  {
+    title: "DevOps & Deployment",
+    description:
+      "I ensure smooth deployment and scalability of applications using Docker and Kubernetes, along with cloud services like AWS. My expertise in Nginx and Git helps optimize server management and version control, while experience in CMS development allows for efficient content management solutions. specialize in building scalable web applications using React, Redux, and JavaScript for dynamic front-end experiences, while leveraging Django and Node.js for robust back-end solutions. With experience in PostgreSQL and MongoDB, I design efficient database structures to support data-driven applications.",
+    skills: ["Docker, Kubernetes, AWS", "Nginx, Git, CMS Development"],
+    mediaSrc: "assets/videos/video1.mov",
+  },
+  {
+    title: "Branding, Marketing & Content Strategy",
+    description:
+      " With over 5 years of experience in hotel branding and digital marketing, I have successfully developed and executed branding strategies, website and booking system development, and campaign management. My ability to integrate technology with marketing ensures impactful digital solutions that align with brand positioning. in building scalable web applications using React, Redux, and JavaScript for dynamic front-end experiences, while leveraging Django and Node.js for robust back-end solutions. With experience in PostgreSQL and MongoDB, I design efficient database structures to support data-driven applications.",
+    skills: [
+      "Hotel branding, digital platforms, & strategy",
+      "Website & booking system development",
+      "Content creation, campaign execution, & brand positioning",
+    ],
+    mediaSrc: "assets/videos/video1.mov",
+  },
+];
